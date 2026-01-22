956403eb8785a59832ba1e0714987f4814c63696
yiyuanyang2003 <yiyuanyang2003@qq.com>
2026-01-22 11:29:50 +0800

    Update Supabase URL and key in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,12 @@
 // assets/js/config.js
 
 // 这里填你的真实 Key
-const SUPABASE_URL = 'https://你的项目ID.supabase.co';
-const SUPABASE_KEY = '你的ANON_KEY';
+const SUPABASE_URL = 'https://tjvzdlecdlukiovefuli.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_fPIRRu-i8BLh3FW5SDKzmQ_zCKvbsjk';
 
 // 初始化客户端
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // 可以在控制台打印一下，确认加载成功
+
 console.log('Supabase 已在 config.js 中初始化');
